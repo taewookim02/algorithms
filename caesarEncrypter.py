b8ef6554a0e898aede64148165f53c4a26d6fb84
taewookim02 <taewookim02@gmail.com>
2022-04-22 23:44:51 +0800

    updating

---
+++ caesarEncrypter.py
@@ -0,0 +1,15 @@
+# string for index
+low = "abcdefghijklmnopqrstuvwxyz"
+cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# input string
+myStr = str(input('Enter your message: '))
+encStr = ""
+# UI
+print("Encrypting...")
+
+# encrypting
+for letters in myStr:
+    print(letters)
+
+# use str index
---
+++ numberCalc.py
@@ -0,0 +1,28 @@
+
+ng = 0
+tae = 0
+goh = 0
+lim = 0
+
+# Lim's peer
+ng += 10
+tae += 9
+goh += 8
+
+# Goh's peer
+tae += 10
+lim += 9
+ng += 8
+
+# Ng's peer
+lim += 10
+tae += 9
+goh += 8
+
+# Tae's peer
+goh += 10
+ng += 9
+lim += 8
+
+
+print("Ng:", ng, "\nTae:", tae, "\nGoh:", goh, "\nLim:", lim)
