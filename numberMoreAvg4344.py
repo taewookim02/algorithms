e8b017cb068c9bf3479a927c09dc1361b73d329e
taewookim02 <taewookim02@gmail.com>
2022-04-29 00:38:01 +0800

    baekjoon

---
+++ numberMoreAvg4344.py
@@ -0,0 +1,13 @@
+N = int(input())
+myList = []
+for i in range(N):
+    myList.append(list(map(int, input().split())))
+
+# myList = [myList[0].split("," for item in myList)]
+newList = []
+for item in myList:
+    newList.append(item[0].split(","))
+
+print(myList)
+
+print("\n", newList)
