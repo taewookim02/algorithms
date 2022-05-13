8f93f0b2b7dc69c3ee9df47012c184498cfb9e0f
taewookim02 <taewookim02@gmail.com>
2022-05-13 23:07:40 +0800

    입출력과 사칙연산

--- baekjoon/입출력과 사칙연산/1000.js
+++ baekjoon/입출력과 사칙연산/1000.js
@@ -1,11 +1,14 @@
 /*
 https://stackoverflow.com/questions/61394928/get-user-input-through-node-js-console
 
+Getting input reference: https://velog.io/@dishate/%EB%B0%B1%EC%A4%80-javascript-node.js-%EC%9E%85%EB%A0%A5%EB%B0%9B%EA%B8%B0
+
 
 
 */
 // n, n<
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().split(' ');
 
-console.log(Number(input[0]) + Number(input[1]));
+console.log(input);
--- baekjoon/입출력과 사칙연산/18108.js
+++ baekjoon/입출력과 사칙연산/18108.js
@@ -2,4 +2,5 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim();
 
-console.log(`${input}??!`);
+const changeYear = 543;
+console.log(+input - changeYear);
