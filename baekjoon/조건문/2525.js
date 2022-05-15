596c1d6541439c0046d5d113ce472c455a3f5683
taewookim02 <taewookim02@gmail.com>
2022-05-15 22:31:24 +0800

    조건문

--- baekjoon/조건문/2525.js
+++ baekjoon/조건문/2525.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split(' ');
-
-console.log(input);
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+let [H, M] = input[0].split(' ').map(Number);
+const cookingMin = Number(input[1]);
 
 // calculate time
-
-// end time
+if (cookingMin + M < 60) {
+  M += cookingMin;
+} else if (cookingMin + M > 60) {
+  H += Math.trunc((cookingMin + M) / 60);
+  if (H > 24) H = H % 24;
+  M = (cookingMin + M) % 60;
+} else if (cookingMin + M === 60) {
+  H++;
+  M = 0;
+}
+if (H === 24) {
+  H = 0;
+}
+console.log(H, M);
