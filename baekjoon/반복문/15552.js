3bfd44624be8532d6fcfccfdb12388b982890785
taewookim02 <taewookim02@gmail.com>
2022-05-21 01:17:35 +0800

    반복문

--- baekjoon/반복문/15552.js
+++ baekjoon/반복문/15552.js
@@ -3,3 +3,8 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 let answer = '';
+for (let i = 1; i < input.length; i++) {
+  let [a, b] = input[i].split(' ').map(Number);
+  answer += a + b + '\n';
+}
+console.log(answer);
