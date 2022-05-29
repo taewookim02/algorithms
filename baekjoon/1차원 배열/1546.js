e54a40083bf34fc22d0c4012bbf851f15eae0a8a
Tae Woo Kim <taewookim02@gmail.com>
2022-05-29 22:02:17 +0800

    [solve] 1차원 배열

--- baekjoon/1차원 배열/1546.js
+++ baekjoon/1차원 배열/1546.js
@@ -2,21 +2,9 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-const avg = input[1].split(' ').map(Number);
-const M = Math.max(...avg);
+const arr = input[1].split(' ').map(Number);
+const M = Math.max(...arr);
 // test / M * 100 = new score
-const newAvg = avg.map(val => {
-  // if (val !== M) return (val / M) * 100;
-  return (val / M) * 100;
-});
+const newAvg = arr.map(val => (val / M) * 100);
 
 console.log(newAvg.reduce((acc, val) => acc + val) / newAvg.length);
-
-// const N = +input[0];
-// const arr = input[1].split(' ').map(Number);
-// const M = Math.max(...arr);
-
-// for (let i = 0; i < N; i++) {
-//   arr[i] = (arr[i] / M) * 100;
-// }
-// console.log(arr.reduce);
