62759482d424bd30f9281c3a910c7d7d90e1da64
Tae Woo Kim <taewookim02@gmail.com>
2022-06-05 20:36:20 +0800

    [solve] 문자열

--- baekjoon/문자열/1157.js
+++ baekjoon/문자열/1157.js
@@ -3,3 +3,21 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().toUpperCase();
 
 // use objects
+const count = {};
+for (let i = 0; i < input.length; i++) {
+  count[`${input[i]}`] = 0;
+}
+for (let i = 0; i < input.length; i++) {
+  count[`${input[i]}`]++;
+}
+
+// get max used char
+const max = Math.max(...Object.values(count));
+
+// filtered array
+const values = Object.values(count).filter(val => val >= max);
+
+// printing answer
+values.length > 1
+  ? console.log('?')
+  : console.log(Object.keys(count).find(key => count[key] === max));
