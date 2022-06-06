48bd20de38efbd6e36690a48ebc9108f3ded8fc0
Tae Woo Kim <taewookim02@gmail.com>
2022-06-07 01:18:45 +0800

    [solve] 문자열

--- baekjoon/문자열/5622.js
+++ baekjoon/문자열/5622.js
@@ -1,8 +1,10 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim();
+
+/*
+// console.log(input);
 let time;
-console.log(input);
 
 const dialLetterPairs = {
   // pair ABCs to numbers
@@ -15,13 +17,50 @@ const dialLetterPairs = {
   8: ['T', 'U', 'V'],
   9: ['W', 'X', 'Y', 'Z'],
 };
-console.log(dialLetterPairs);
+// console.log(dialLetterPairs[2]);
 
-// Loop over input
-for (let char of input) {
-  // find corresponding number and add to time
-  // console.log(Object.values(dialLetterPairs);
+// time taken for 1 === 2;
+// time taken for 2 === 3;
+// if W -> time += 9 + 1
+
+// const objVal = Object.entries(dialLetterPairs);
+let counter = 0;
+for (const [key, value] of Object.entries(dialLetterPairs)) {
+  if (value.indexOf(input[counter]) > -1) {
+    // time += +key + 1;
+    console.log('HO!');
+  }
+  counter++;
 }
-console.log(dialLetterPairs.includes(['P', 'Q', 'R', 'S']));
+console.log(time);
 
-// https://stackoverflow.com/questions/8217419/how-to-determine-if-javascript-array-contains-an-object-with-an-attribute-that-e
+// const [k, v] = Object.entries(dialLetterPairs);
+
+// console.log(objVal);
+*/
+
+let dial = {
+  ABC: 3,
+  DEF: 4,
+  GHI: 5,
+  JKL: 6,
+  MNO: 7,
+  PQRS: 8,
+  TUV: 9,
+  WXYZ: 10,
+};
+
+// for (let x in dial) {
+//   console.log(x);
+//   console.log(dial[x]);
+// }
+
+let time = 0;
+for (let i = 0; i < input.length; i++) {
+  for (let x in dial) {
+    if (x.includes(input[i])) {
+      time += dial[x];
+    }
+  }
+}
+console.log(time);
