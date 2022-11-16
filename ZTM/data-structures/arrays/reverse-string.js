7269e7fc12ffbfaa7e431588faaf6e6b96c2f979
Tae Woo Kim <taewookim02@gmail.com>
2022-11-16 23:40:26 +0800

    solve: reverse a string

---
+++ ZTM/data-structures/arrays/reverse-string.js
@@ -0,0 +1,10 @@
+const case1 = 'Hi my name is Taewoo';
+// ooweaT si eman ym iH
+
+function reverse(str) {
+  // reverse a string
+  const myList = str.split('').reverse().join('');
+  console.log(myList);
+}
+
+reverse(case1);
