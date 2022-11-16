926958aec2f2d45dd5dd5a1810ac1c1bea2f0769
Tae Woo Kim <taewookim02@gmail.com>
2022-11-16 23:53:06 +0800

    answers: reverse string using es6, for loops, and more

--- ZTM/data-structures/arrays/reverse-string.js
+++ ZTM/data-structures/arrays/reverse-string.js
@@ -1,4 +1,4 @@
-const case1 = 'Hi my name is Taewoo';
+/*const case1 = 'Hi my name is Taewoo';
 // ooweaT si eman ym iH
 
 function reverse(str) {
@@ -8,3 +8,32 @@ function reverse(str) {
 }
 
 reverse(case1);
+*/
+
+function reverse(str) {
+  // check input
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'weird input';
+  }
+
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for (let i = totalItems; i >= 0; i--) {
+    backwards.push(str[i]);
+  }
+
+  return backwards.join('');
+}
+
+function reverse2(str) {
+  if (!str || str.length < 2 || typeof str !== 'string') {
+    return 'weird input';
+  }
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = str => str.split('').reverse().join('');
+
+const reverse4 = str => [...str].reverse().join('');
+
+console.log(reverse4('Hi my name is taewoo'));
